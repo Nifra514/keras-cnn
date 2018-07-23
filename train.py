16cce80cc9e2d0572eb26fac227ca738860decbb
nifra514 <nifras.ict>
2018-07-23 21:10:31 +0530

    updated

--- train.py
+++ train.py
@@ -4,116 +4,105 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.optimizers import Adam
 from keras import backend as K
-import logging
 
-logging.basicConfig(filename='Log/train_log.log', level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
-try:
-        img_width, img_height = 200, 250
-
-        try: 
-                if K.image_data_format() == 'channels_first':
-                        input_shape = (3, img_width, img_height)
-                else:
-                        input_shape = (img_width, img_height, 3)
-        
+img_width, img_height = 200, 250
 
 
-                #input
-                model = Sequential()
-                model.add(Conv2D(32, (3, 3), input_shape=input_shape,padding='same'))
-                model.add(Dropout(0.2))
-                #model.add(MaxPooling2D(pool_size=(2, 2)))
+if K.image_data_format() == 'channels_first':
+        input_shape = (3, img_width, img_height)
+else:
+        input_shape = (img_width, img_height, 3)
 
-                #first convo
-                model.add(Conv2D(32, (3, 3), padding='valid'))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2, 2)))
-                model.add(Dropout(0.2))
 
-                #second convo
-                model.add(Conv2D(64, (3, 3), padding='valid'))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2, 2)))
-                model.add(Dropout(0.2))
 
-                #third convo
-                model.add(Conv2D(64, (3, 3), padding='valid'))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2, 2)))
-                model.add(Dropout(0.2))
+#input
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape,padding='same'))
+model.add(Dropout(0.2))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
 
-                #fourth convo
-                model.add(Conv2D(128, (3, 3), padding='valid'))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2, 2)))
-                model.add(Dropout(0.2))
+#first convo
+model.add(Conv2D(32, (3, 3), padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-                #fifth convo
-                model.add(Conv2D(128, (3, 3), padding='valid'))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2, 2)))
-                model.add(Dropout(0.2))
+#second convo
+model.add(Conv2D(64, (3, 3), padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-                #fully connected
-                model.add(Flatten())
-                model.add(Dense(256))
-                model.add(Activation('relu'))
-                model.add(Dropout(0.5))
-                model.add(Dense(3))
-                model.add(Activation('softmax'))
+#third convo
+model.add(Conv2D(64, (3, 3), padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-                
-                model.compile(loss='categorical_crossentropy',
-                        optimizer=Adam(lr=1e-3),
-                        metrics=['categorical_accuracy'])
+#fourth convo
+model.add(Conv2D(128, (3, 3), padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-                train_datagen = ImageDataGenerator(
-                        rescale=1./255,
-                        rotation_range=20,
-                        width_shift_range=0.2,
-                        height_shift_range=0.2,
-                        shear_range=0.2,
-                        zoom_range=0.2,
-                        horizontal_flip=True)
+#fifth convo
+model.add(Conv2D(128, (3, 3), padding='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-                test_datagen = ImageDataGenerator(rescale=1. / 255)
+#fully connected
+model.add(Flatten())
+model.add(Dense(256))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(33))
+model.add(Activation('softmax'))
 
-                training = train_datagen.flow_from_directory(
-                        'Dataset/train',
-                        target_size=(img_width, img_height),
-                        batch_size=16,
-                        class_mode='categorical',
-                        # color_mode='grayscale',
-                        # save_to_dir='preview',
-                        # save_format='jpeg'
-                        )
 
-                testing = test_datagen.flow_from_directory(
-                        'Dataset/test',
-                        target_size=(img_width, img_height),
-                        batch_size=16,
-                        class_mode='categorical',
-                        # color_mode='grayscale'
-                        )
-        
+model.compile(loss='categorical_crossentropy',
+        optimizer=Adam(lr=1e-3),
+        metrics=['categorical_accuracy'])
 
-                model.fit_generator(
-                        training,
-                        steps_per_epoch=25,
-                        epochs=20,
-                        validation_data=testing,
-                        validation_steps=15,
-                        # callbacks=[checkpointer]
-                        )
-        except ValueError:
-                logging.error('Input_Error: {}'.format('Invalid input found in the file'))
-                
-        # to save model weights
-        model.save_weights('./models/trained_model4.h5')
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
 
-        logging.info('Model: {}'.format('Trained model generated'))
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-except KeyboardInterrupt:
+training = train_datagen.flow_from_directory(
+        'Dataset/train',
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='categorical',
+        # color_mode='grayscale',
+        # save_to_dir='preview',
+        # save_format='jpeg'
+        )
 
-        logging.error('Error: {}'.format('Operation cancelled by user'))
+testing = test_datagen.flow_from_directory(
+        'Dataset/test',
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='categorical',
+        # color_mode='grayscale'
+        )
+
+
+model.fit_generator(
+        training,
+        steps_per_epoch=125,
+        epochs=25,
+        validation_data=testing,
+        validation_steps=31,
+        # callbacks=[checkpointer]
+        )
+
+
+# to save model weights
+model.save_weights('./models/trained_model.h5')
