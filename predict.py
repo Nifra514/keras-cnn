6b17b09c04b7c8db86da99e63a2804cf90ed1ce1
nifra514 <nifras.ict>
2018-07-24 08:27:57 +0530

    Edited

--- predict.py
+++ predict.py
@@ -40,39 +40,36 @@ try:
             0: '1',
             1: '2',
             2: '3',
-            # 3: '4',
-            # 4: '5',
-            # 5: '6',
-            # 6: '7',
-            # 7: '8',
-            # 8: '9',
-            # 9: 'A',
-            # 10: 'B',
-            # 11: 'C',
-            # 12: 'D',
-            # 13: 'E',
-            # 14: 'F',
-            # 15: 'G',
-            # 16: 'H',
-            # 17: 'I',
-            # 18: 'J',
-            # 19: 'K',
-            # 20: 'L',
-            # 21: 'M',
-            # 22: 'N',
-            # 23: 'O',
-            # 24: 'P',
-            # 25: 'Q',
-            # 26: 'R',
-            # 27: 'S',
-            # 28: 'T',
-            # 29: 'U',
-            # 30: 'V',
-            # 31: 'W',
-            # 32: 'X',
-            # 33: 'Y',
-            # 34: 'Z',
-            
+            3: '4',
+            4: '5',
+            5: '6',
+            6: '7',
+            7: '8',
+            8: '9',
+            9: 'A',
+            10: 'B',
+            11: 'C',
+            12: 'D',
+            13: 'E',
+            14: 'F',
+            15: 'G',
+            16: 'H',
+            17: 'I',
+            18: 'K',
+            19: 'L',
+            20: 'M',
+            21: 'N',
+            22: 'O',
+            23: 'P',
+            24: 'Q',
+            25: 'R',
+            26: 'S',
+            27: 'T',
+            28: 'U',
+            29: 'V',
+            30: 'W',
+            31: 'X',
+            32: 'Y',
             
         }
 
@@ -122,14 +119,14 @@ try:
         model.add(Dense(256))
         model.add(Activation('relu'))
         model.add(Dropout(0.5))
-        model.add(Dense(3))
+        model.add(Dense(33))
         model.add(Activation('softmax'))
 
     
 
         #download and load model 
         # utility.load_model()
-        model.load_weights('./models/trained_model.h5')
+        model.load_weights('./models/trained_model.hdf')
 
     except ValueError:
         logging.error('Class_Error: {}'.format('Invalid number of classes'))
